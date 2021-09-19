48048f8bfbe2d9ad1ede3e9edac110ee361d002b
Kárpáti Tamás <tamas.nandor.karpati@gmail.com>
2021-09-19 18:36:51 +0200

    changed file upload to dropzone

--- src/Components/UI/FileUpload.js
+++ src/Components/UI/FileUpload.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+import Dropzone from 'react-dropzone';
 import Button from './Button';
 import './FileUpload.css';
 
 function FileUpload(props) {
+	const [file, set_file] = useState(null);
 	return (
 		<div className='file-wrapper'>
-			<input
-				type='file'
-				name='file'
-				title='asd'
-				className='file-upload'
-				onChange={(e) => props.onChange(e.target.files[0])}
-			/>
+			<Dropzone
+				onDrop={(acceptedFiles) => {
+					props.onChange(acceptedFiles[0]);
+					set_file(acceptedFiles[0]);
+				}}
+			>
+				{({ getRootProps, getInputProps }) => (
+					<section>
+						<div {...getRootProps()}>
+							<input {...getInputProps()} />
+							<p>
+								{file ? file.name : 'Kérjük válassz egy fájlt, vagy húzd ide!'}
+							</p>
+						</div>
+					</section>
+				)}
+			</Dropzone>
+
 			<Button small hasContainer onClick={props.onSubmit}>
 				Feltöltés
 			</Button>
